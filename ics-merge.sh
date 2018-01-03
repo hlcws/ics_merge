043640129dbb316d79b7564939ef11bb14784d27
root <root@gosuland.org>
2018-01-03 19:28:06 +0100

    correct ICAL formatting (CR, whitespace, ...)

--- ics-merge.sh
+++ ics-merge.sh
@@ -1,37 +1,37 @@
 #!/bin/sh
-wget -O ranbat-raw.ics https://forum.hardedge.org/calendar/index.php?calendar-export/11/
-wget -O turnier-raw.ics https://forum.hardedge.org/calendar/index.php?calendar-export/7/
-wget -O session-raw.ics https://forum.hardedge.org/calendar/index.php?calendar-export/6/
+#Get calendars
+wget -O ranbat.ics https://forum.hardedge.org/calendar/index.php?calendar-export/11/
+wget -O turnier.ics https://forum.hardedge.org/calendar/index.php?calendar-export/7/
+wget -O session.ics https://forum.hardedge.org/calendar/index.php?calendar-export/6/
 
 
-#Add category and make UID unique with prefix
-awk '/END:VEVENT/{print"CATEGORIES: Turnier"}1' turnier-raw.ics > turnier-categories.ics
-sed s/@forum.hardedge.org/@turnier.hardedge.org/ < turnier-categories.ics > turnier.ics
-awk '/END:VEVENT/{print"CATEGORIES: Ranbat"}1' ranbat-raw.ics > ranbat-categories.ics
-sed s/@forum.hardedge.org/@ranbat.hardedge.org/ < ranbat-categories.ics > ranbat.ics
-awk '/END:VEVENT/{print"CATEGORIES: Session"}1' session-raw.ics > session-categories.ics
-sed s/@forum.hardedge.org/@session.hardedge.org/ < session-categories.ics > session.ics
+#Add category to calendars and make UID unique with prefix
+gawk -i inplace '/END:VEVENT/{print"CATEGORIES: Turnier"}1' turnier.ics
+sed -i 's/@forum.hardedge.org/@turnier.hardedge.org/' turnier.ics
+awk -i inplace '/END:VEVENT/{print"CATEGORIES: Ranbat"}1' ranbat.ics
+sed -i 's/@forum.hardedge.org/@Ranbat.hardedge.org/' ranbat.ics
+awk -i inplace '/END:VEVENT/{print"CATEGORIES: Session"}1' session.ics
+sed -i 's/@forum.hardedge.org/@Session.hardedge.org/' session.ics
 
-
-#init merged ICS
+#init merged calendar
 echo "BEGIN:VCALENDAR
 PRODID:-//Hardedge Merge//
 VERSION:2.0
 X-WR-CALNAME:Hardedge" > he-merge.ics
 
-#append all ics
+#append all calendars
 awk '/BEGIN:VEVENT/,/END:VEVENT/' turnier.ics >> he-merge.ics
 awk '/BEGIN:VEVENT/,/END:VEVENT/' ranbat.ics >> he-merge.ics
 awk '/BEGIN:VEVENT/,/END:VEVENT/' session.ics >> he-merge.ics
 
-#testmerge, use dos2unix instead
-#cat turnier-categories.ics ranbat-categories.ics session-categories.ics >> he-merge2.ics
-
 #close the ics
 echo "END:VCALENDAR" >> he-merge.ics
 
-#get carriage return right
+#correct formatting (CR)
 dos2unix he-merge.ics
+
+#remove blank lines
+gawk -i inplace 'NF' he-merge.ics
 
 #publish
 cp he-merge.ics /var/www/wordpress/
